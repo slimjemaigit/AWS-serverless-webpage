1f884eab61835b5cb3a6d0de391e5c0963fd2f00
slimjemaigit <slim.speedknees@gmail.com>
2024-05-11 03:00:00 +0100

    deploying api gateway

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.getElementById('getDataBtn').addEventListener('click', async function() {
     try {
-        const response = await fetch('https://your-api-gateway-url.com/example'); // Replace with your actual API Gateway endpoint URL
+        const response = await fetch('https://wj6d0f5da9.execute-api.eu-west-3.amazonaws.com/test'); // Replace with your actual API Gateway endpoint URL
         const data = await response.json();
         document.getElementById('response').innerText = JSON.stringify(data);
     } catch (error) {
